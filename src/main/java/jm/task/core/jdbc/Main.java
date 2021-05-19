b8acec050309a1dfd38249ceb02f4cc7a80857f9
KagamiNo <Qursed888>
2021-05-19 09:33:52 +0300

    Complete task

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,9 +1,11 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class Main {
     public static void main(String[] args) {
-        
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,6 +1,10 @@
 package jm.task.core.jdbc.util;
 
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,17 +12,32 @@ import java.sql.SQLException;
 public class Util {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/myowndb";
-    private static final String USERNAME = "Kiririrm";
+    private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
-    public static Connection getConnection(){
+    public static Connection getConnection() {
         Connection connection = null;
         try {
             Class.forName(DRIVER);
-            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
-        }catch (ClassNotFoundException | SQLException e){
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Connection Error!");
         }
         return connection;
+    }
+
+    public static SessionFactory getSession() {
+        SessionFactory sessionFactory = null;
+        try {
+            sessionFactory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+
+            System.out.println("Сессия установлена");
+        } catch (Exception e) {
+            System.out.println("Сессия не установлена");
+        }
+        return sessionFactory;
     }
 }
